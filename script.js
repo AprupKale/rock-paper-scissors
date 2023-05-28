3dfa732c8538d5e34e12b9f5a4bb36e1179b3563
AprupKale <aprup.kale@gmail.com>
2023-05-28 10:30:31 +0530

    Edit script.js, style.css and index.html

--- script.js
+++ script.js
@@ -39,7 +39,61 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-    
+    function updateScoreDisplay() {
+        playerScoreDisplay.textContent = playerScore;
+        computerScoreDisplay.textContent = computerScore;
+    }
+
+    let playerScore = 0;
+    let computerScore = 0;
+
+    const playerScoreDisplay = document.createElement('p');
+    document.querySelector('#player').appendChild(playerScoreDisplay);
+    const computerScoreDisplay = document.createElement('p');
+    document.querySelector('#computer').appendChild(computerScoreDisplay);
+    updateScoreDisplay();
+
+    const roundMessage = document.createElement('h2');
+    document.querySelector('body').insertBefore(roundMessage, document.querySelector('.play-again-button'));
+
+    const victoryDisplay = document.createElement('h1');
+    document.querySelector('body').insertBefore(victoryDisplay, document.querySelector('.play-again-button'));
+
+    document
+        .querySelectorAll('.button-section > .button')
+        .forEach(button => button.addEventListener('click', e => {
+            if (playerScore === 5 || computerScore === 5) return;
+
+            let message = playRound(e.target.id, getComputerChoice());
+            roundMessage.textContent = message;
+
+            if (message.startsWith('You Win!')) playerScore++;
+            if (message.startsWith('You Lose!')) computerScore++;
+
+            if (playerScore == 5) {
+                roundMessage.textContent = '';
+                victoryDisplay.textContent = 'Player Wins!';
+            }
+
+            if (computerScore == 5) {
+                roundMessage.textContent = '';
+                victoryDisplay.textContent = 'Computer Wins!';
+            }
+
+            updateScoreDisplay();
+        }));
+
+    document
+        .querySelector('.play-again-button > .button')
+        .addEventListener('click', e => {
+            playerScore = 0;
+            computerScore = 0;
+            
+            victoryDisplay.textContent = '';
+            roundMessage.textContent = '';
+
+            updateScoreDisplay();
+        })
 }
 
 game();
